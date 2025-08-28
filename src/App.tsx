1690020224b99b413e1de64dc65b779f9f3826c7
your_username <malahat.m82@gmail.com>
2025-08-28 10:17:02 +0200

    fixing homepage address.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 // App.tsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/projects";
 import Resume from "./pages/Resume";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-		 <Route path="/resume" element={<Resume />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/resume" element={<Resume />} />
+    </Routes>
   );
 }
 
