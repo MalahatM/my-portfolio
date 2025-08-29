f271061ed6adfe57d3122a002228b796cfef0a57
Shahryar Pi <dev.shahryar@gmail.com>
2025-08-29 14:03:19 +0330

    fix resume

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ function Home() {
     setShakingButton(index);
 
     if (buttons[index] === "Resume") {
-      window.open("/Resume.pdf", "_blank");
+      window.open("/my-portfolio/resume.pdf", "_blank");
     } else if (buttons[index] === "Projects") {
       navigate("/projects");
     } else if (buttons[index] === "About") {
